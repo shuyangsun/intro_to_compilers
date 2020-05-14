1bc1599bc67cfe2f2e6f6f57480cdbe182bc9d52
Shuyang Sun <sunbuffett@gmail.com>
2020-05-13 20:16:46 -0400

    Delte accidentally commited target folder.

--- token_scanner/target/debug/deps/token_scanner-7cdc8cf981695e66.d
+++
@@ -1,5 +0,0 @@
-/Users/shuyangsun/Developer/intro_to_compilers/token_scanner/target/debug/deps/token_scanner-7cdc8cf981695e66.rmeta: src/main.rs
-
-/Users/shuyangsun/Developer/intro_to_compilers/token_scanner/target/debug/deps/token_scanner-7cdc8cf981695e66.d: src/main.rs
-
-src/main.rs:
--- token_scanner/target/debug/token_scanner.d
+++
@@ -1,1 +0,0 @@
-/Users/shuyangsun/Developer/intro_to_compilers/token_scanner/target/debug/token_scanner: /Users/shuyangsun/Developer/intro_to_compilers/token_scanner/src/main.rs
