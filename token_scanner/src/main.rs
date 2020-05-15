3e712ba03f66a3b42c8b66804b4ad8fb17c50913
Shuyang Sun <sunbuffett@gmail.com>
2020-05-14 23:13:11 -0400

    Use map macro.

--- token_scanner/src/main.rs
+++ token_scanner/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use token_scanner::{FiniteAutomaton, NFA};
 
 macro_rules! set {
@@ -7,25 +7,34 @@ macro_rules! set {
     }};
 }
 
+macro_rules! map(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+            m
+        }
+     };
+);
+
 fn main() {
     let alphabets = set!(['0', '1']);
     let states = set!([0, 1, 2]);
     let accepted_states = set!([0, 2]);
-
-    let mut zero_map = HashMap::new();
-    zero_map.insert('0', set!([1]));
-
-    let mut one_map = HashMap::new();
-    one_map.insert('1', set!([2]));
-
-    let mut two_map = HashMap::new();
-    two_map.insert('0', set!([2]));
-    two_map.insert('1', set!([1]));
-
-    let mut transition_map = HashMap::new();
-    transition_map.insert(0, zero_map);
-    transition_map.insert(1, one_map);
-    transition_map.insert(2, two_map);
+    let transition_map: HashMap<i32, HashMap<char, HashSet<i32>>> = map!(
+        0 => map!(
+            '0' => set!([1])
+        ),
+        1 => map!(
+            '1' => set!([2])
+        ),
+        2 => map!(
+            '0' => set!([2]),
+            '1' => set!([1])
+        )
+    );
 
     let nfa = NFA::from(states, alphabets, 0, accepted_states, transition_map);
     let strings = vec!["", "0", "01", "010", "0100", "0101"];
